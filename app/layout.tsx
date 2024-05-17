22ef3a0ef9185b11ec43d79c0ee51d76337bec67
Sahil <dshiv867@gmail.com>
2024-05-17 19:37:04 +0530

    initiated project

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Park Management',
+  description: 'A Park Management system',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning={true}>
+      <body>{children}</body>
+    </html>
+  );
+}
